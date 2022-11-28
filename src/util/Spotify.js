c32a1c282912df6350ac137bf19caa8b070922dd
Justin Abellera <justin.c.abellera@gmail.com>
2022-11-27 22:18:25 -0800

    Spotify buildout progress

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,11 +1,22 @@
+const clientId = '1ac157e5ebb3462e8d26cd279d52aed0';
+const redirectURI = 'http://localhost:3000/';
 let aToken;
+let tknExp;
 const Spotify = {
     getAccessToken() {
         if(aToken) {
             return aToken
-        } else {
-            null
+        } else if(window.location.href.match(/access_token=([^&]*)/)) {
+            aToken = window.location.href.match(/access_token=([^&]*)/);
+            tknExp = window.location.href.match(/expires_in=([^&]*)/);
+            window.setTimeout(() => aToken = '', tknExp * 1000);
+            window.history.pushState('Access Token', null, '/');
+            window.location = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
         }
+    },
+    async search(term) {
+        const results = await fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`);
+        return results
     }
 }
 
